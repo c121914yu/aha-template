9a96a3a634cb963a3bc9c772052169502848d878
金鱼 <545436317@qq.com>
2021-11-12 20:07:52 +0800

    feat: 模板初始化

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+import '../css/index.css'
+import IMG from '../imgs/test.png'
+let width, height
+
+let mycanvas, ctx
+
+let name
+
+window.onload = async function() {
+  initCanvas()
+  initWidth()
+  name = prompt('请输入名字')
+  await loadImg()
+  drawText()
+  canvas2Img()
+}
+/**
+ * canvas初始化
+ */
+function initCanvas() {
+  mycanvas = document.getElementById('canvas')
+  ctx = mycanvas.getContext('2d')
+}
+/**
+ * 初始化宽度高
+ */
+function initWidth() {
+  width = screen.width
+  height = width * 0.75
+
+  /* 画布宽高比dom节点3倍，保障画质 */
+  mycanvas.setAttribute('width', width * 3)
+  mycanvas.setAttribute('height', height * 3)
+
+  mycanvas.style.width = `${ width }px`
+  mycanvas.style.height = `${ height }px`
+}
+
+/**
+ * 加载图片
+ */
+function loadImg() {
+  return new Promise((resolve) => {
+    const img = new Image()
+
+    img.src = IMG
+    img.onload = function() {
+      // 将图片画到canvas上面上去！
+      ctx.drawImage(img, 0, 0, mycanvas.width, mycanvas.height)
+      resolve()
+    }
+    img.error = function() {
+      alert('加载图片失败')
+    }
+  })
+}
+
+/**
+ * 绘制文字
+ */
+function drawText() {
+  ctx.font = '40px "微软雅黑"'
+  ctx.fillStyle = 'white'
+  ctx.textBaseline = 'top'
+  ctx.textAlign = 'left'
+  ctx.fillText(name, 10, 10)
+}
+
+/**
+ * 转成图片并销毁canvas
+ */
+function canvas2Img() {
+  const img = document.createElement('img')
+
+  img.src = canvas.toDataURL('image/png')
+  img.className = 'postcard'
+  document.querySelector('body').appendChild(img)
+  // setTimeout(() => {
+  //     document.querySelector("body").removeChild(document.getElementById("loading"))
+  // },1000)
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  mode: 'production', // 开发模式
+  devServer: {
+    port: 3000,
+  },
+  entry: './src/js/index.js', // 入口起点
+  output: {
+    filename: 'js/[hash:8].js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [miniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: 'imgs/[hash:8].[ext]',
+          esModule: false,
+        },
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: '明信片制作',
+      cdn: {
+        css: [],
+        js: [],
+        deferJs: []
+      }
+    }),
+    new miniCssExtractPlugin({
+      filename: 'css/[hash:8].css' // 输出文件名
+    }),
+    () => isProd ? new CleanWebpackPlugin() : null, // 清除上次打包的文件
+  ],
+  performance: {
+    hints: false // 取消性能提醒
+  },
+}
